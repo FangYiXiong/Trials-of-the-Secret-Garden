25bc1750df9117294736188ccfe0e6be0aad641a
方一雄 <haydenfang@icloud.com>
2016-08-10 11:48:32 +0800

    Add the localized string to the game. It works.

--- Trials of the Secret Garden/Trials of the Secret Garden tvOS/GameScene.swift
+++ Trials of the Secret Garden/Trials of the Secret Garden tvOS/GameScene.swift
@@ -8,18 +8,42 @@
 
 import SpriteKit
 
-class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
+class GameScene: SGScene {
+  override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, Swift!"
+        myLabel.text = lt("Language")
         myLabel.fontSize = 65
         myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         
         self.addChild(myLabel)
     }
+  
+  // MARK: Life Cycle
    
-    override func update(currentTime: CFTimeInterval) {
+  override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
+  
+  // MARK: Responders
+  
+  override func screenInteractionStarted(location: CGPoint) {
+    // Overridden by Subclass
+  }
+  
+  override func screenInteractionMoved(location: CGPoint) {
+    // Overridden by Subclass
+  }
+  
+  override func screenInteractionEnded(location: CGPoint) {
+    // Overridden by Subclass
+  }
+  
+  override func buttonEvent(enent: String, velocity: Float, pushedOn: Bool) {
+    // Overridden by Subclass
+  }
+  
+  override func stickEvent(event: String, point: CGPoint) {
+    // Overridden by Subclass
+  }
 }
